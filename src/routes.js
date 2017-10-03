35a35365a75d781b1e7da0dc4a0939a79d57bf49
Mohit Deshpande <mohitdeshpande@icloud.com>
2017-10-03 15:02:06 -0400

    add routes configuration file

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+/**
+ * This is the routes configuration for components
+ */
+import App from './App.js';
+
+const routes = [
+  {
+    path: '/',
+    component: App
+  }
+];
+
+export default routes;
