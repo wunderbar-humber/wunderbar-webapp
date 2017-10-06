3a844b450d5aed5ad024141fed99652d78f9ea87
Mohit Deshpande <mohitdeshpande@icloud.com>
2017-10-05 20:01:28 -0400

    materialize-css is now added as a node module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../node_modules/materialize-css/dist/css/materialize.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
