709ba1ca60d1725a6665169d488b56c4163ed6fc
Aditya Gulhane <addi2140@gmail.com>
2017-10-08 19:37:52 -0400

    "TroubleShoot: Components path being reached but not displayed"

---
+++ src/components/profile/About.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+class About extends Component {
+  render() {
+    return (
+      <Router>
+        <body>
+          <div class="container">
+            <div class="col s2">
+              <img
+                src="https://www.pinterest.ca/pin/766315692816936128/"
+                alt=""
+                class="circle responsive-img"
+                height="500"
+                width="500"
+              />
+            </div>
+          </div>
+        </body>
+        <About />
+      </Router>
+    );
+  }
+}
+
+export default About;
