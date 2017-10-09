1ea9d53599cc6b78b9731c7938ec8dfe95cd5896
Mohit Deshpande <mohitdeshpande@icloud.com>
2017-10-09 09:53:10 -0400

    Fixed the routing issue, The router was rendering inside the <nav> tag that's why nothing was visible. Also pinterest image doesn't work because of pinterest restrictions

--- src/components/profile/PastOrders.js
+++ src/components/profile/PastOrders.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class PastOrders extends Component {
   render() {
     return (
-      <Router>
-        <div class="container">
-          <div class="col s2">
-            <img
-              src="https://www.pinterest.ca/pin/766315692816936128/"
-              alt=""
-              class="circle responsive-img"
-              height="500"
-              width="500"
-            />
-          </div>
+      <div className="container">
+        <div className="col s2">
+          <h1>Hello From PastOrders</h1>
+          <img
+            src="https://www.pinterest.ca/pin/766315692816936128/"
+            alt=""
+            className="circle responsive-img"
+            height="500"
+            width="500"
+          />
         </div>
-      </Router>
+      </div>
     );
   }
 }
