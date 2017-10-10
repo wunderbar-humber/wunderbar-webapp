32f040416d32a9ebf2a8c764a5915347dfb076eb
Mykyta Plakhotin <nikita.plakhotin@gmail.com>
2017-10-10 17:41:48 -0400

    merge conflict with develop

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Restaurant from './components/restaurant/Restaurant';
 import logo from './logo.svg';
 import Navbar from './components/common/Navbar';
 import NavbarNew from './components/common/NavbarNew';
-<<<<<<< HEAD
+import Card from './components/common/Card';
 
 /**
  * This component is the root/main component of this React application
@@ -15,17 +15,17 @@ import NavbarNew from './components/common/NavbarNew';
  * @class App
  * @extends {Component}
  */
-=======
-import Card from './components/common/Card';
->>>>>>> Card
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
       <Router>
         <div>
+          <NavbarNew
+            profileName="Nikita"
+            profileImage="http://materializecss.com/images/yuna.jpg"
+          />
           {/* temporary navbar */}
-          <nav>
+          {/* <nav>
             <div className="nav-wrapper container">
               <Link to="/" className="brand-logo">
                 Logo
@@ -39,47 +39,26 @@ class App extends Component {
                 </li>
               </ul>
             </div>
-          </nav>
+          </nav> */}
 
           {/* Application body */}
           <Route exact path="/" component={Home} />
           <Route path="/profile" component={Profile} />
           <Route path="/restaurant" component={Restaurant} />
 
+          <Card
+            restaurantName="Isshin"
+            restaurantImage="https://static.wixstatic.com/media/ca289c_6f22b05392fd43b0a71a30f8ec041607~mv2.png_256"
+            restaurantCategory="Oriental"
+            restaurantAddress="College Street 232, Toronto, ON"
+            restaurantPhone="(111)-111-1111"
+            restaurantPrice="$$"
+          />
+
           {/* footer */}
           <Footer />
-=======
-      <div className="App">
-        {/* <Navbar
-          profileName="Mohit"
-          profileImage="http://materializecss.com/images/yuna.jpg"
-        /> */}
-        <NavbarNew
-          profileName="Nikita"
-          profileImage="http://materializecss.com/images/yuna.jpg"
-        />
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
->>>>>>> sort of fixed navbar
         </div>
-<<<<<<< HEAD
       </Router>
-=======
-
-        <Card
-          restaurantName="Isshin"
-          restaurantImage="https://static.wixstatic.com/media/ca289c_6f22b05392fd43b0a71a30f8ec041607~mv2.png_256"
-          restaurantCategory="Oriental"
-          restaurantAddress="College Street 232, Toronto, ON"
-          restaurantPhone="(111)-111-1111"
-          restaurantPrice="$$"
-        />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
->>>>>>> Card
     );
   }
 }
