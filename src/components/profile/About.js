bcd87f80d69e05616d109e9ff4beca93104a6f54
Aditya Gulhane <addi2140@gmail.com>
2017-10-14 19:07:27 -0400

    "Changes in Profile.js added cost var"

--- src/components/profile/About.js
+++ src/components/profile/About.js
@@ -5,25 +5,20 @@ class About extends Component {
   render() {
     return (
       <div className="container">
-        <div class="row valign-wrapper">
-          <div className="col s2">
-            <img
-              src=""
-              alt=""
-              className="circle responsive-img"
-              height="500"
-              width="500"
-            />
-          </div>
-          <div className="row">
-            <div className="col s12 m5">
-              <div className="card-panel black z-depth-5">
-                <span className="white-text">
-                  I am a very simple card. I am good at containing small bits of
-                  information. I am convenient because I require little markup
-                  to use effectively. I am similar to what is called a panel in
-                  other frameworks.
-                </span>
+        <div className="Profile">
+          <div className="App" />
+          );
+          <div class="row valign-wrapper">
+            <div className="row">
+              <div className="col s12 m5">
+                <div className="card-panel black z-depth-5">
+                  <span className="white-text">
+                    I am a very simple card. I am good at containing small bits
+                    of information. I am convenient because I require little
+                    markup to use effectively. I am similar to what is called a
+                    panel in other frameworks.
+                  </span>
+                </div>
               </div>
             </div>
           </div>
