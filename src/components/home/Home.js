eceec5f9bad4669cf15f4ee291621d33ab416367
Mohit Deshpande <mohitdeshpande@icloud.com>
2017-10-16 13:46:27 -0400

    finish layout

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -14,27 +14,46 @@ class Home extends Component {
     return (
       <main>
         <div className="row" id="home-page">
-          <div className="col m4 l3 hide-on-small-only" id="side-nav">
+          <div className="col m3 l2 hide-on-small-only" id="side-nav">
             <ul>
               <li>
-                <a className="waves-effect waves-teal btn-flat">Restaurants</a>
+                <a className="btn">All</a>
               </li>
               <li>
-                <a className="waves-effect waves-teal btn-flat">Cafes</a>
+                <a className="btn">Restaurants</a>
               </li>
               <li>
-                <a className="waves-effect waves-teal btn-flat">
-                  Pubs &amp; Bars
-                </a>
+                <a className="btn active">Cafes</a>
+              </li>
+              <li>
+                <a className="btn">Pubs &amp; Bars</a>
               </li>
               <div className="divider" />
               <li>
-                <a className="waves-effect waves-teal btn-flat">Settings</a>
+                <a className="btn">Settings</a>
               </li>
             </ul>
           </div>
-          <div className="col s12 m8 l9" id="main-content">
+          <div className="col s12 m9 l10" id="main-content">
+            <div className="row" id="tag-area">
+              <div className="chip">
+                All
+                <i className="close material-icons">close</i>
+              </div>
+              <div className="chip">
+                Restaurant
+                <i className="close material-icons">close</i>
+              </div>
+            </div>
             <div className="row">
+              <Card
+                restaurantName="Isshin"
+                restaurantImage="https://static.wixstatic.com/media/ca289c_6f22b05392fd43b0a71a30f8ec041607~mv2.png_256"
+                restaurantCategory="Oriental"
+                restaurantAddress="College Street 232, Toronto, ON"
+                restaurantPhone="(111)-111-1111"
+                restaurantPrice="$$"
+              />
               <Card
                 restaurantName="Isshin"
                 restaurantImage="https://static.wixstatic.com/media/ca289c_6f22b05392fd43b0a71a30f8ec041607~mv2.png_256"
