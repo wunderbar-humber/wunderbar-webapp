9e078ad376aa8b66fe5ed1d7be42bef3ebdb627f
Aditya Gulhane <addi2140@gmail.com>
2017-12-03 16:32:35 -0500

    "Finished footer with modal"

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import './Footer.css';
+import ReactModal from 'react-modal';
 
 class Footer extends Component {
   render() {
@@ -16,19 +18,64 @@ class Footer extends Component {
               <h5 className="white-text">Links</h5>
               <ul>
                 <li>
-                  <a className="grey-text text-lighten-3" href="#modal1">
+                  <a
+                    onClick={this.handleOpenModal}
+                    className="grey-text text-lighten-3"
+                  >
                     About Wunderbar
                   </a>
+                  <ReactModal
+                    isOpen={this.state.showModal1}
+                    contentLabel="Modal #1 Global Style Override Example"
+                    onRequestClose={this.handleCloseModal}
+                  >
+                    <p>
+                      Wunderbar is a app which will provide you status of your
+                      favourite restaurants,live tracking of how busy that place
+                      is, want to reach there call an uber from our app itself
+                      and better yet want to rate your favourite joints.
+                      Wunderbar is the place for you.
+                    </p>
+                    <button onClick={this.handleCloseModal}>Close</button>
+                  </ReactModal>
                 </li>
                 <li>
-                  <a className="grey-text text-lighten-3" href="#!">
+                  <a
+                    onClick={this.handleOpenModal2}
+                    className="grey-text text-lighten-3"
+                  >
                     About Us
                   </a>
+                  <ReactModal
+                    isOpen={this.state.showModal2}
+                    contentLabel="Modal #2 Global Style Override Example"
+                    onRequestClose={this.handleCloseModal2}
+                  >
+                    <p>Modal Text</p>
+                    <button onClick={this.handleCloseModal2}>Close</button>
+                  </ReactModal>
                 </li>
                 <li>
-                  <a className="grey-text text-lighten-3" href="#!">
-                    Contact Us
+                  <a
+                    onClick={this.handleOpenModal3}
+                    className="grey-text text-lighten-3"
+                  >
+                    Contact us
                   </a>
+                  <ReactModal
+                    isOpen={this.state.showModal3}
+                    contentLabel="Modal #3 Global Style Override Example"
+                    onRequestClose={this.handleCloseModal3}
+                  >
+                    <p>
+                      Wunderbar is a app which will provide you status of your
+                      favourite restaurants,live tracking of how busy that place
+                      is, want to reach there call an uber from our app itself
+                      and better yet want to rate your favourite joints.
+                      Wunderbar is the place for you.
+                    </p>
+                    <button onClick={this.handleCloseModal3}>Close</button>
+                  </ReactModal>
                 </li>
               </ul>
             </div>
@@ -41,17 +88,44 @@ class Footer extends Component {
     );
   }
 
-  constructor(props) {
-    super(props);
-    this.state = { isModalOpen: false };
+  constructor() {
+    super();
+    this.state = {
+      showModal: false,
+      showModal2: false,
+      showModal3: false
+    };
+
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleOpenModal2 = this.handleOpenModal2.bind(this);
+    this.handleOpenModal3 = this.handleOpenModal3.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+    this.handleCloseModal2 = this.handleCloseModal2.bind(this);
+    this.handleCloseModal3 = this.handleCloseModal3.bind(this);
   }
 
-  openModal() {
-    this.setState({ isModalOpen: true });
+  handleOpenModal() {
+    this.setState({ showModal: true });
   }
 
-  closeModal() {
-    this.setState({ isModalOpen: false });
+  handleOpenModal2() {
+    this.setState({ showModal2: true });
+  }
+
+  handleOpenModal3() {
+    this.setState({ showModal2: true });
+  }
+
+  handleCloseModal() {
+    this.setState({ showModal: false });
+  }
+
+  handleCloseModal2() {
+    this.setState({ showModal2: false });
+  }
+
+  handleCloseModal3() {
+    this.setState({ showModal2: false });
   }
 }
 
