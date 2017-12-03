3d8d19a619e5e558dbdce75bfe06928f0a40b2a7
Aditya Gulhane <addi2140@gmail.com>
2017-12-03 14:58:45 -0500

    "installed modal class in package and made changes to footer"

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -3,68 +3,54 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <div>
-        <footer className="page-footer">
-          <div className="container">
-            <div className="row">
-              <div className="col l6 s12">
-                <h5 className="white-text">Footer Content</h5>
-                <p className="grey-text text-lighten-4">
-                  You can use rows and columns here to organize your footer
-                  content.
-                </p>
-              </div>
-              <div className="col l4 offset-l2 s12">
-                <h5 className="white-text">Links</h5>
-                <ul>
-                  <li>
-                    <a className="grey-text text-lighten-3" href="#modal1">
-                      Link 1
-                    </a>
-                  </li>
-                  <li>
-                    <a className="grey-text text-lighten-3" href="#!">
-                      Link 2
-                    </a>
-                  </li>
-                  <li>
-                    <a className="grey-text text-lighten-3" href="#!">
-                      Link 3
-                    </a>
-                  </li>
-                  <li>
-                    <a className="grey-text text-lighten-3" href="#!">
-                      Link 4
-                    </a>
-                  </li>
-                </ul>
-              </div>
+      <footer className="page-footer">
+        <div className="container">
+          <div className="row">
+            <div className="col l6 s12">
+              <h5 className="white-text">Wunderbar</h5>
+              <p className="grey-text text-lighten-4">
+                If you liked us, give a moment to look at our information.
+              </p>
             </div>
-          </div>
-          <div className="footer-copyright">
-            <div className="container">
-              © 2014 Copyright Text
-              <a className="grey-text text-lighten-4 right" href="#!">
-                More Links
-              </a>
+            <div className="col l4 offset-l2 s12">
+              <h5 className="white-text">Links</h5>
+              <ul>
+                <li>
+                  <a className="grey-text text-lighten-3" href="#modal1">
+                    About Wunderbar
+                  </a>
+                </li>
+                <li>
+                  <a className="grey-text text-lighten-3" href="#!">
+                    About Us
+                  </a>
+                </li>
+                <li>
+                  <a className="grey-text text-lighten-3" href="#!">
+                    Contact Us
+                  </a>
+                </li>
+                <li>
+                  <a className="grey-text text-lighten-3" href="#!">
+                    Link 4
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
         <div className="footer-copyright">
-          <div className="container">
-            © 2014 Copyright Text
-            <a className="grey-text text-lighten-4 right" href="#!">
-              More Links
-            </a>
-          </div>
+          <div className="container">Wunderbar © 2017</div>
         </div>
       </footer>
     );
   }
+
   constructor(props) {
     super(props);
     this.state = { isModalOpen: false };
   }
+
   openModal() {
     this.setState({ isModalOpen: true });
   }
--- src/components/profile/Board.js
+++ src/components/profile/Board.js
@@ -26,6 +26,61 @@ class Board extends Component {
                 </div>
               </div>
             </div>
+            <div className="col s12 m6">
+              <div className="card z-depth-5">
+                <div className="card-image">
+                  <img
+                    src="https://i.pinimg.com/736x/4a/a8/fa/4aa8fad6d6793a2a5e2599e321ccb6b9.jpg"
+                    alt={this.props}
+                  />
+                  <span className="card-title" />
+                  <a className="btn-floating halfway-fab waves-effect waves-light red">
+                    <i class="material-icons">+</i>
+                  </a>
+                </div>
+                <div className="card-content">
+                  <p>Victoria Lake,Yoho National Park,Canada</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col s4">
+          <div className="row">
+            <div className="col s12 m6">
+              <div className="card z-depth-5">
+                <div className="card-image">
+                  <img
+                    src="https://500px.com/photo/117487879/rocky-shore-in-georgian-bay-by-elena-elisseeva"
+                    alt={this.props}
+                  />
+                  <span className="card-title" />
+                  <a className="btn-floating halfway-fab waves-effect waves-light red">
+                    <i class="material-icons">+</i>
+                  </a>
+                </div>
+                <div className="card-content">
+                  <p>Canadian Mountain Range</p>
+                </div>
+              </div>
+            </div>
+            <div className="col s12 m6">
+              <div className="card z-depth-5">
+                <div className="card-image">
+                  <img
+                    src="https://i.pinimg.com/564x/b3/ec/40/b3ec40693b18dee972a4a91982e7e7c6.jpg"
+                    alt={this.props}
+                  />
+                  <span className="card-title">Mello Melly</span>
+                  <a className="btn-floating halfway-fab waves-effect waves-light red">
+                    <i class="material-icons">+</i>
+                  </a>
+                </div>
+                <div className="card-content">
+                  <p>Now Here's a Photo of Mel Gibson for you to enjoy!</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
